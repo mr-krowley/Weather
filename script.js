929b2448f960ee43eb1047e1a967590831474e69
Viktor <midyukinv@mail.ru>
2023-04-16 14:07:19 +0300

    add success()

---
+++ script.js
@@ -0,0 +1,23 @@
+let container = document.querySelector(".container");
+navigator.geolocation.getCurrentPosition(success, error);
+
+const KEY_KEY = "8a7e864e357a0fb36c793bdf68016d3f";
+
+function success(position) {
+  //декомпозиция координат в отдельные переменные
+  const { longitude, latitude } = position.coords;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${KEY_KEY}`
+  )
+    .then((promis) => promis.json())
+    .then((data) => {
+      container.innerHTML = `<p class ="weather">${Math.round(
+        data.main.temp - 273
+      )}</p>
+                            <p class="city">${data.name}</p>  
+                            <a class="link" href=""></a>
+      `;
+    });
+}
+
+function error() {}
